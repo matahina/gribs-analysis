237c8a017c6f0238ec7cf54db35bf761c44f2cc7
matahina <matahina@posteo.net>
2026-05-17 20:44:38 +0200

    Better disconnections handling

--- scripts/dl/get_ecmwf.py
+++ scripts/dl/get_ecmwf.py
@@ -13,73 +13,77 @@ client = Client(source="azure")
 
 os.chdir("../../data/ecmwf/")
 
-dled = False
 for retries in range(1,50):
-    if dled:
+    try resume:
         break
-    try:
-        client.retrieve(
-            date=model_date,
-            time=model_run,
-            step=list(range(0,144,3))+list(range(144,366,6)),
-            stream="enfo",
-            type="pf",
-            param=["gh"],
-            levelist=[500],
-            target=str(sys.argv[1])+str(sys.argv[2])+"data_hgt.grib2",
-        )
-        dled = True
-    except Exception as error:
-        dled = False
-        with open('../logs/'+model_date+'.log', 'a') as errlog:
-            errlog.write('ERROR: '+error)
-            errlog.write('\n')
-        time.sleep(100)
+    except:
+        try:
+            resume = client.retrieve(
+                date=model_date,
+                time=model_run,
+                step=list(range(0,144,3))+list(range(144,366,6)),
+                stream="enfo",
+                type="pf",
+                param=["gh"],
+                levelist=[500],
+                target=str(sys.argv[1])+str(sys.argv[2])+"data_hgt.grib2",
+            )
+        except Exception as error:
+            with open('../logs/'+model_date+'.log', 'a') as errlog:
+                errlog.write('ERROR: '+error)
+                errlog.write('\n')
+            time.sleep(100)
 
-dled = False
-for retries in range(1,50):
-    if dled:
-        break
-    try:
-        client.retrieve(
-            date=model_date,
-            time=model_run,
-            step=list(range(0,144,3))+list(range(144,366,6)),
-            stream="enfo",
-            type="pf",
-            param=["t"],
-            levelist=[850],
-            target=str(sys.argv[1])+str(sys.argv[2])+"data_talt.grib2",
-        )
-        dled = True
-    except Exception as error:
-        dled = False
-        with open('../logs/'+model_date+'.log', 'a') as errlog:
-            errlog.write('ERROR: '+error)
-            errlog.write('\n')
-        time.sleep(100)
+try resume:
+    del resume
+except:
+    pass
 
-dled = False
 for retries in range(1,50):
-    if dled:
+    try resume:
         break
-    try:
-        client.retrieve(
-            date=model_date,
-            time=model_run,
-            step=list(range(0,144,3))+list(range(144,366,6)),
-            stream="enfo",
-            type="pf",
-            param=["2t", "tp"],
-            target=str(sys.argv[1])+str(sys.argv[2])+"data_tsolpp.grib2",
-        )
-        dled = True
-    except Exception as error:
-        dled = False
-        with open('../logs/'+model_date+'.log', 'a') as errlog:
-            errlog.write('ERROR: '+error)
-            errlog.write('\n')
-        time.sleep(100)
+    except:
+        try:
+            client.retrieve(
+                date=model_date,
+                time=model_run,
+                step=list(range(0,144,3))+list(range(144,366,6)),
+                stream="enfo",
+                type="pf",
+                param=["t"],
+                levelist=[850],
+                target=str(sys.argv[1])+str(sys.argv[2])+"data_talt.grib2",
+            )
+        except Exception as error:
+            with open('../logs/'+model_date+'.log', 'a') as errlog:
+                errlog.write('ERROR: '+error)
+                errlog.write('\n')
+            time.sleep(100)
+
+try resume:
+    del resume
+except:
+    pass
+
+for retries in range(1,50):
+    try resume:
+        break
+    except:
+        try:
+            client.retrieve(
+                date=model_date,
+                time=model_run,
+                step=list(range(0,144,3))+list(range(144,366,6)),
+                stream="enfo",
+                type="pf",
+                param=["2t", "tp"],
+                target=str(sys.argv[1])+str(sys.argv[2])+"data_tsolpp.grib2",
+            )
+        except Exception as error:
+            with open('../logs/'+model_date+'.log', 'a') as errlog:
+                errlog.write('ERROR: '+error)
+                errlog.write('\n')
+            time.sleep(100)
 
 
 
