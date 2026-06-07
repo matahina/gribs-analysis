f92ad66db8ea94608bfbac15c93174c42fa4803d
matahina <matahina@posteo.net>
2026-06-07 13:49:21 +0200

    step_by_step

--- scripts/dl/get_ecmwf.py
+++ scripts/dl/get_ecmwf.py
@@ -11,223 +11,89 @@ from ecmwf.opendata import Client
 
 os.chdir("../../data/ecmwf/")
 
-for retries in range(1,50):
-    try:
-        resume
-        break
-    except:
+for the_step in list(range(0,144,3))+list(range(144,366,6)):
+    for retries in range(1,50):
         try:
-            client = Client(source="azure")
-            resume = client.retrieve(
-                date=model_date,
-                time=model_run,
-                step=list(range(0,144,3)),
-                stream="enfo",
-                type="pf",
-                param=["gh"],
-                levelist=[500],
-                target=model_date+model_run+"data_hgt_a.grib2",
-            )
+            resume
+            break
         except:
-            with open('../logs/'+model_date+'.log', 'a') as errlog:
-                errlog.write("error")
-                errlog.write('\n')
-            time.sleep(100)
+            try:
+                client = Client(source="aws")
+                resume = client.retrieve(
+                    date=model_date,
+                    time=model_run,
+                    step=the_step,
+                    stream="enfo",
+                    type="pf",
+                    param=["gh"],
+                    levelist=[500],
+                    target=model_date+model_run+"data_hgt_%03d.grib2" % the_step,
+                )
+            except:
+                with open('../logs/'+model_date+'.log', 'a') as errlog:
+                    errlog.write("error")
+                    errlog.write('\n')
+                time.sleep(100)
 
-try:
-    del resume
-except:
-    pass
-
-for retries in range(1,50):
     try:
-        resume
-        break
+        del resume
     except:
+        pass
+
+    for retries in range(1,50):
         try:
-            client = Client(source="azure")
-            resume = client.retrieve(
-                date=model_date,
-                time=model_run,
-                step=list(range(144,366,6)),
-                stream="enfo",
-                type="pf",
-                param=["gh"],
-                levelist=[500],
-                target=model_date+model_run+"data_hgt_b.grib2",
-            )
+            resume
+            break
         except:
-            with open('../logs/'+model_date+'.log', 'a') as errlog:
-                errlog.write("error")
-                errlog.write('\n')
-            time.sleep(100)
+            try:
+                client = Client(source="aws")
+                resume = client.retrieve(
+                    date=model_date,
+                    time=model_run,
+                    step=the_step,
+                    stream="enfo",
+                    type="pf",
+                    param=["t"],
+                    levelist=[850],
+                    target=model_date+model_run+"data_talt_%03d.grib2" % the_step,
+                )
+            except:
+                with open('../logs/'+model_date+'.log', 'a') as errlog:
+                    errlog.write("error")
+                    errlog.write('\n')
+                time.sleep(100)
 
-try:
-    del resume
-except:
-    pass
-
-for retries in range(1,50):
     try:
-        resume
-        break
+        del resume
     except:
+        pass
+
+    for retries in range(1,50):
         try:
-            client = Client(source="azure")
-            resume = client.retrieve(
-                date=model_date,
-                time=model_run,
-                step=list(range(0,144,3)),
-                stream="enfo",
-                type="pf",
-                param=["t"],
-                levelist=[850],
-                target=model_date+model_run+"data_talt_a.grib2",
-            )
+            resume
+            break
         except:
-            with open('../logs/'+model_date+'.log', 'a') as errlog:
-                errlog.write("error")
-                errlog.write('\n')
-            time.sleep(100)
+            try:
+                client = Client(source="aws")
+                resume = client.retrieve(
+                    date=model_date,
+                    time=model_run,
+                    step=the_step,
+                    stream="enfo",
+                    type="pf",
+                    param=["tp","2t"],
+                    target=model_date+model_run+"data_tsolpp_%03d.grib2" % the_step,
+                )
+            except:
+                with open('../logs/'+model_date+'.log', 'a') as errlog:
+                    errlog.write("error")
+                    errlog.write('\n')
+                time.sleep(100)
 
-try:
-    del resume
-except:
-    pass
-
-for retries in range(1,50):
     try:
-        resume
-        break
+        del resume
     except:
-        try:
-            client = Client(source="azure")
-            resume = client.retrieve(
-                date=model_date,
-                time=model_run,
-                step=list(range(144,366,6)),
-                stream="enfo",
-                type="pf",
-                param=["t"],
-                levelist=[850],
-                target=model_date+model_run+"data_talt_b.grib2",
-            )
-        except:
-            with open('../logs/'+model_date+'.log', 'a') as errlog:
-                errlog.write("error")
-                errlog.write('\n')
-            time.sleep(100)
-
-try:
-    del resume
-except:
-    pass
-
-for retries in range(1,50):
-    try:
-        resume
-        break
-    except:
-        try:
-            client = Client(source="azure")
-            resume = client.retrieve(
-                date=model_date,
-                time=model_run,
-                step=list(range(0,144,3)),
-                stream="enfo",
-                type="pf",
-                param=["tp"],
-                target=model_date+model_run+"data_pp_a.grib2",
-            )
-        except:
-            with open('../logs/'+model_date+'.log', 'a') as errlog:
-                errlog.write("error")
-                errlog.write('\n')
-            time.sleep(100)
-
-try:
-    del resume
-except:
-    pass
-
-for retries in range(1,50):
-    try:
-        resume
-        break
-    except:
-        try:
-            client = Client(source="azure")
-            resume = client.retrieve(
-                date=model_date,
-                time=model_run,
-                step=list(range(144,366,6)),
-                stream="enfo",
-                type="pf",
-                param=["tp"],
-                target=model_date+model_run+"data_pp_b.grib2",
-            )
-        except:
-            with open('../logs/'+model_date+'.log', 'a') as errlog:
-                errlog.write("error")
-                errlog.write('\n')
-            time.sleep(100)
-
-try:
-    del resume
-except:
-    pass
-
-for retries in range(1,50):
-    try:
-        resume
-        break
-    except:
-        try:
-            client = Client(source="azure")
-            resume = client.retrieve(
-                date=model_date,
-                time=model_run,
-                step=list(range(0,144,3)),
-                stream="enfo",
-                type="pf",
-                param=["2t"],
-                target=model_date+model_run+"data_tsol_a.grib2",
-            )
-        except:
-            with open('../logs/'+model_date+'.log', 'a') as errlog:
-                errlog.write("error")
-                errlog.write('\n')
-            time.sleep(100)
-
-try:
-    del resume
-except:
-    pass
-
-for retries in range(1,50):
-    try:
-        resume
-        break
-    except:
-        try:
-            client = Client(source="azure")
-            resume = client.retrieve(
-                date=model_date,
-                time=model_run,
-                step=list(range(144,366,6)),
-                stream="enfo",
-                type="pf",
-                param=["2t"],
-                target=model_date+model_run+"data_tsol_b.grib2",
-            )
-        except:
-            with open('../logs/'+model_date+'.log', 'a') as errlog:
-                errlog.write("error")
-                errlog.write('\n')
-            time.sleep(100)
-
-
-
+        pass
 
 
 
