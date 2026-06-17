99c5e465bb9d1b78c37ee54c1bcc22e8f9f7d079
matahina <matahina@posteo.net>
2026-06-17 12:30:39 +0200

    more threaded

--- scripts/dl/get_extract_aigefs.py
+++ scripts/dl/get_extract_aigefs.py
@@ -36,7 +36,6 @@ class CustomThread(Thread):
         return self._return
 
 
-
 import xarray as xr
 import math
 import pandas as pd
@@ -74,74 +73,74 @@ donneesrun = pd.DataFrame({'runs': [], 'dates': [], 'profile': [], 'geop': [], '
 first_try = True
 
 n_pert = 31
-ech_range = list(range(6,385,6))
+ech_range = list(range(6,391,6))
 
 for ech in ech_range:
     print(ech, "out of",ech_range[-1])
     thread_list = []
-    for sc in range(1,n_pert):
-        for level in ["pres","sfc"]:
+    names_list = []
+    if ech < 390:
+        for sc in range(1,n_pert):
+            for level in ["pres","sfc"]:
+                full_url = 'https://nomads.ncep.noaa.gov/pub/data/nccf/com/aigefs/prod/aigefs.'+model_date+'/'+model_run+'/mem'+"{:03d}".format(sc)+'/model/atmos/grib2/aigefs.t'+model_run+'z.'+level+'.f'+"{:03d}".format(ech)+".grib2"
 
 
-            full_url = 'https://nomads.ncep.noaa.gov/pub/data/nccf/com/aigefs/prod/aigefs.'+model_date+'/'+model_run+'/mem'+"{:03d}".format(sc)+'/model/atmos/grib2/aigefs.t'+model_run+'z.'+level+'.f'+"{:03d}".format(ech)+".grib2"
+                file_name = model_date+"_"+model_run+"_"+level+"_"+"{:03d}".format(sc)+"_"+"{:03d}".format(ech)+".grib2"
+                names_list.append(file_name)
+
+                thread = Thread(target=common.get_ens, args=(full_url,file_name,model_date,model_name))
+                thread_list.append(thread)
+
+    dataliste = [[]] * (n_pert-1)
+
+    if ech > 6:
 
 
-            file_name = model_date+"_"+model_run+"_"+level+"_"+"{:03d}".format(sc)+"_"+"{:03d}".format(ech)+".grib2"
+        for sc in range(1, n_pert):
 
-
-            thread = Thread(target=common.get_ens, args=(full_url,file_name,model_date,model_name))
+            thread = CustomThread(target=common.data_ens_ai, args=(sc,ech-6,model_date,model_name,model_run,profiles))
             thread_list.append(thread)
 
     for thread in thread_list:
         thread.start()
     for thread in thread_list:
-        thread.join()
-    time.sleep(0.1)
-
-    dataliste = [[]] * 30
-
-
-    thread_list = []
-
-    for sc in range(1, n_pert):
-
-        thread = CustomThread(target=common.data_ens_ai, args=(sc,ech,model_date,model_name,model_run,profiles))
-        thread_list.append(thread)
-
-    for thread in thread_list:
-        thread.start()
-    i=0
-    for thread in thread_list:
-        dataliste[i]=thread.join()
-        print(dataliste[i])
-        i+=1
+        res=thread.join()
+        if not res is None:
+            if res != "":
+                dataliste[res[0]-1] = res[1]
     time.sleep(0.1)
 
 
-    if first_try:
-        frames = dataliste
-        first_try = False
-    else:
-        frames = [donneesjour] + dataliste
+    if ech > 6:
+        if first_try:
+            frames = dataliste
+            first_try = False
+        else:
+            frames = [donneesjour] + dataliste
 
-    try:
-        new_donneesjour = pd.concat([df for df in frames if not df.empty], ignore_index=True)
-        donneesjour = new_donneesjour
-    except:
-        pass
+        try:
+            new_donneesjour = pd.concat([df for df in frames if not df.empty], ignore_index=True)
+            donneesjour = new_donneesjour
+        except:
+            pass
 
-    print(donneesjour)
+        print(donneesjour)
 
     liste = os.listdir(os.curdir)
 
     for item in liste:
-        if item.endswith(".idx") or item.endswith(".grib2"):
-            os.remove(os.path.join(os.curdir, item))
+        if item.endswith(".grib2"):
+            if not item in names_list:
+                os.remove(os.path.join(os.curdir, item))
 
 
 
 
+liste = os.listdir(os.curdir)
 
+for item in liste:
+    if item.endswith(".idx") or item.endswith(".grib2"):
+        os.remove(os.path.join(os.curdir, item))
 
 
 
--- scripts/dl/get_extract_gefs.py
+++ scripts/dl/get_extract_gefs.py
@@ -72,77 +72,77 @@ donneesrun = pd.DataFrame({'runs': [], 'dates': [], 'profile': [], 'geop': [], '
 
 first_try = True
 
-
 n_pert = 31
-ech_range = list(range(3,193,3)) + list(range(198,385,6))
-
-
+ech_range = list(range(3,193,3)) + list(range(198,391,6))
 
 for ech in ech_range:
     print(ech, "out of",ech_range[-1])
     thread_list = []
-    for sc in range(1,n_pert):
+    names_list = []
+    if ech < 390:
+        for sc in range(1,n_pert):
+            full_url = 'https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.'+model_date+'/'+model_run+'/atmos/pgrb2ap5/gep'+"{:02d}".format(sc)+'.t'+model_run+'z.pgrb2a.0p50.f'+"{:03d}".format(ech)
 
 
+            file_name = model_date+"_"+model_run+"_"+"{:03d}".format(sc)+"_"+"{:03d}".format(ech)+".grb2"
+            names_list.append(file_name)
 
+            thread = Thread(target=common.get_ens, args=(full_url,file_name,model_date,model_name))
+            thread_list.append(thread)
 
+    dataliste = [[]] * (n_pert-1)
 
-        full_url = 'https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.'+model_date+'/'+model_run+'/atmos/pgrb2ap5/gep'+"{:02d}".format(sc)+'.t'+model_run+'z.pgrb2a.0p50.f'+"{:03d}".format(ech)
+    if ech > 3:
+        if ech >193:
+            delta=6
+        else:
+            delta=3
 
+        for sc in range(1, n_pert):
 
-
-
-        file_name = model_date+"_"+model_run+"_"+"{:03d}".format(sc)+"_"+"{:03d}".format(ech)+".grb2"
-
-
-        thread = Thread(target=common.get_ens, args=(full_url,file_name,model_date,model_name))
-        thread_list.append(thread)
-
-    for thread in thread_list:
-        thread.start()
-    for thread in thread_list:
-        thread.join()
-    time.sleep(0.1)
-
-    dataliste = [[]] * 30
-
-
-    thread_list = []
-
-    for sc in range(1, n_pert):
-
-        thread = CustomThread(target=common.data_ens, args=(sc,ech,model_date,model_name,model_run,profiles))
-        thread_list.append(thread)
+            thread = CustomThread(target=common.data_ens, args=(sc,ech-delta,model_date,model_name,model_run,profiles))
+            thread_list.append(thread)
 
     for thread in thread_list:
         thread.start()
-    i=0
     for thread in thread_list:
-        dataliste[i]=thread.join()
-        print(dataliste[i])
-        i+=1
+        res=thread.join()
+        if not res is None:
+            if res != "":
+                dataliste[res[0]-1] = res[1]
     time.sleep(0.1)
 
 
-    if first_try:
-        frames = dataliste
-        first_try = False
-    else:
-        frames = [donneesjour] + dataliste
+    if ech > 3:
+        if first_try:
+            frames = dataliste
+            first_try = False
+        else:
+            frames = [donneesjour] + dataliste
 
-    try:
-        new_donneesjour = pd.concat([df for df in frames if not df.empty], ignore_index=True)
-        donneesjour = new_donneesjour
-    except:
-        pass
+        try:
+            new_donneesjour = pd.concat([df for df in frames if not df.empty], ignore_index=True)
+            donneesjour = new_donneesjour
+        except:
+            pass
 
-    print(donneesjour)
+        print(donneesjour)
 
     liste = os.listdir(os.curdir)
 
     for item in liste:
-        if item.endswith(".idx") or item.endswith(".grb2"):
-            os.remove(os.path.join(os.curdir, item))
+        if item.endswith(".grib2"):
+            if not item in names_list:
+                os.remove(os.path.join(os.curdir, item))
+
+
+
+
+liste = os.listdir(os.curdir)
+
+for item in liste:
+    if item.endswith(".idx") or item.endswith(".grib2"):
+        os.remove(os.path.join(os.curdir, item))
 
 
 
