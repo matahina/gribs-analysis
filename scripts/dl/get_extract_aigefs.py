01040e2dc0559d4f4c2179404409ecc00238261a
matahina <matahina@posteo.net>
2026-06-17 08:57:13 +0200

    threading noaa

--- scripts/dl/get_extract_aigefs.py
+++ scripts/dl/get_extract_aigefs.py
@@ -19,7 +19,22 @@ socket.setdefaulttimeout(60*5)
 
 import requests
 
-import threading
+from threading import Thread
+
+class CustomThread(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args, **self._kwargs)
+
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+
 
 
 import xarray as xr
@@ -59,8 +74,7 @@ donneesrun = pd.DataFrame({'runs': [], 'dates': [], 'profile': [], 'geop': [], '
 first_try = True
 
 n_pert = 31
-
-ech_range = list(range(0,385,6))
+ech_range = list(range(6,385,6))
 
 for ech in ech_range:
     print(ech, "out of",ech_range[-1])
@@ -75,7 +89,7 @@ for ech in ech_range:
             file_name = model_date+"_"+model_run+"_"+level+"_"+"{:03d}".format(sc)+"_"+"{:03d}".format(ech)+".grib2"
 
 
-            thread = threading.Thread(target=common.get_ens, args=(full_url,file_name,model_date,model_name))
+            thread = Thread(target=common.get_ens, args=(full_url,file_name,model_date,model_name))
             thread_list.append(thread)
 
     for thread in thread_list:
@@ -84,87 +98,39 @@ for ech in ech_range:
         thread.join()
     time.sleep(0.1)
 
+    dataliste = [[]] * 30
+
+
+    thread_list = []
+
     for sc in range(1, n_pert):
-        print("sc: "+str(sc))
-        donneesrun = pd.DataFrame({'runs': [], 'dates': [], 'profile': [], 'geop': [], 'tempalt': [], 'geop': [], 'tempalt': []})
-        grbfile_a = "%s_%s_pres_%03d_%03d.grib2" % (
-                            model_date,
-                            model_run,
-                            sc,
-                            ech)
-        grbfile_b = "%s_%s_sfc_%03d_%03d.grib2" % (
-                            model_date,
-                            model_run,
-                            sc,
-                            ech)
+
+        thread = CustomThread(target=common.data_ens_ai, args=(sc,ech,model_date,model_name,model_run,profiles))
+        thread_list.append(thread)
+
+    for thread in thread_list:
+        thread.start()
+    i=0
+    for thread in thread_list:
+        dataliste[i]=thread.join()
+        print(dataliste[i])
+        i+=1
+    time.sleep(0.1)
 
 
-        if Path(grbfile_a).is_file() and Path(grbfile_b).is_file():
-            if os.path.getsize(grbfile_a) > 0 and os.path.getsize(grbfile_b) > 0 :
-                try:
-                    ds_grib_a = xr.merge(cfgrib.open_datasets(grbfile_a), combine_attrs='override', compat='override', join='outer')
-                    ds_grib_b = xr.merge(cfgrib.open_datasets(grbfile_b), combine_attrs='override', compat='override', join='outer')
-                    for prof_name, location in profiles.items():
-                        try:
-                            var_hgt = float(ds_grib_a.gh.sel(longitude=location[1], latitude=location[0], isobaricInhPa=500, method='nearest').data) /10
-                        except:
-                            var_hgt = None
-                        try:
-                            var_talt = float(ds_grib_a.t.sel(longitude=location[1], latitude=location[0], isobaricInhPa=850, method='nearest').data) - 273.15
-                        except:
-                            var_talt = None
-                        try:
-                            var_tsol = float(ds_grib_b.t2m.sel(longitude=location[1], latitude=location[0], method='nearest').data) - 273.15
-                        except:
-                            var_tsol = None
-                        try:
-                            var_pp = float(ds_grib_b.tp.sel(longitude=location[1], latitude=location[0], method='nearest').data)
-                        except:
-                            var_pp = None
-                        try:
-                            var_date_a = pd.to_datetime(ds_grib_a.sel(longitude=location[1], latitude=location[0], method='nearest').coords['valid_time'].data, format='%Y-%m-%d %H:%M:%s')
-                        except:
-                            var_date_a = None
-                        try:
-                            var_date_b = pd.to_datetime(ds_grib_b.sel(longitude=location[1], latitude=location[0], method='nearest').coords['valid_time'].data, format='%Y-%m-%d %H:%M:%s')
-                        except:
-                            var_date_b = None
-                        try:
-                            var_run_a = str(pd.to_datetime(ds_grib_a.sel(longitude=location[1], latitude=location[0], method='nearest').coords['time'].data, format='%Y-%m-%d %H:%M:%s'))+ " sc%02d" % (sc)
-                        except:
-                            var_run_a = None
-                        try:
-                            var_run_b = str(pd.to_datetime(ds_grib_a.sel(longitude=location[1], latitude=location[0], method='nearest').coords['time'].data, format='%Y-%m-%d %H:%M:%s'))+ " sc%02d" % (sc)
-                        except:
-                            var_run_b = None
+    if first_try:
+        frames = dataliste
+        first_try = False
+    else:
+        frames = [donneesjour] + dataliste
 
-                        try:
-                            newdata_a = pd.DataFrame({'runs': [var_run_a], 'dates': [var_date_a], 'profile': [prof_name], 'geop': [var_hgt], 'tempalt': [var_talt]})
+    try:
+        new_donneesjour = pd.concat([df for df in frames if not df.empty], ignore_index=True)
+        donneesjour = new_donneesjour
+    except:
+        pass
 
-                            newdata_b = pd.DataFrame({'runs': [var_run_b], 'dates': [var_date_b], 'profile': [prof_name], 'tempsol': [var_tsol], 'precs': [var_pp]})
-
-                            newdata = pd.merge(newdata_a, newdata_b, on=["runs", "dates", "profile"], how = 'outer')
-                        except:
-                            newdata = pd.DataFrame({'runs': [], 'dates': [], 'profile': [], 'geop': [], 'tempalt': [], 'tempsol': [], 'precs': []})
-
-                        frames = [donneesrun,newdata]
-                        try:
-                            new_donneesrun = pd.concat([df for df in frames if not df.empty], ignore_index=True)
-                            donneesrun = new_donneesrun
-                        except:
-                            pass
-                except:
-                    pass
-        if first_try:
-            donneesjour = donneesrun
-            first_try = False
-        else:
-            frames = [donneesjour, donneesrun]
-            try:
-                new_donneesjour = pd.concat([df for df in frames if not df.empty], ignore_index=True)
-                donneesjour = new_donneesjour
-            except:
-                pass
+    print(donneesjour)
 
     liste = os.listdir(os.curdir)
 
